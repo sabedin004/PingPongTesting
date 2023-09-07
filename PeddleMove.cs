80355102cd9fff7a389b9a9716070f3903dbdf26
sabedin004 <144362658+sabedin004@users.noreply.github.com>
2023-09-07 12:17:18 -0400

    Create PeddleMove.cs

---
+++ PeddleMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeddleMove : MonoBehaviour
+{
+    //Variable for Movement
+    public float speed = .10f;
+    public float yBorder = 4.5f;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.O))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + speed);
+        }
+    }
+
+
+
+
+}
